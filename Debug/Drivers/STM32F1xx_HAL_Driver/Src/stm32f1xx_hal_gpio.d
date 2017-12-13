dc67a407729bdb53cf4066fc9e576e4f0626f5d3
Ho Guo Xian <hogouxian@gmail.com>
2017-12-13 15:55:29 +0800

    initialize with motor rampup

---
+++ Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.d
@@ -0,0 +1,65 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Inc/stm32f1xx_hal_conf.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Inc/main.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Inc/stm32f1xx_hal_conf.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Inc/main.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
