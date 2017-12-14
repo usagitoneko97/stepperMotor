febed01a47143d95ab05de794111de62f1825bec
Ho Guo Xian <hogouxian@gmail.com>
2017-12-15 01:03:48 +0800

    implemented constant acceleration rump up of motor
    blocking mode

--- Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.d
+++ Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.d
@@ -1,65 +1,65 @@
 Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.o: \
  ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Inc/stm32f1xx_hal_conf.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Inc/main.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cm3.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
- C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Inc/main.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Inc/stm32f1xx_hal_conf.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Inc/stm32f1xx_hal_conf.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Inc/main.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Inc/main.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cm3.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cm3.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
 
-C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -27,7 +27,7 @@ Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I"C:/Students/Venno/Projects/STM32/StepperMotor/Inc" -I"C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Students/Venno/Projects/STM32/StepperMotor/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I"C:/Users/User_Lenovo/workspace/stepperMotor/Inc" -I"C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/User_Lenovo/workspace/stepperMotor/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
